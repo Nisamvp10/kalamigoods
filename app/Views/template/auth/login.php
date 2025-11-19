2d53638b3a495ae3c50c63efc059f2f0e8297361
Nisam Git <nisamvp10@gmail.com>
2025-11-19 12:20:17 +0530

    title

--- app/Views/template/auth/login.php
+++ app/Views/template/auth/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Task Management System</title>
+    <title>Login </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <link href="<?=site_url('public/assets/src/output.css');?>" rel="stylesheet" >
